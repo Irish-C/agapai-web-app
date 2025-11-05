ee91be7c81e9c29bc3d20a7aa5e7ec2fb90ea93b
IrishC <maryirish.cammagay@gmail.com>
2025-11-05 12:51:57 +0800

    Change to concurrent

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 // Corrected absolute path for global CSS
-import '/assets/styles/index.css'; 
+import './assets/styles/index.css'; 
 
 /**
  * The main entry point for the React application.
